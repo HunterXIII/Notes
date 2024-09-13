4149d63221cc51258e5d3d49eee524930fc2292e
Роман <volkraftroman@gmail.com>
2024-09-13 21:16:07 +0300

    Сохранение_13_09_2024_2116_S

--- pushed.sh
+++ pushed.sh
@@ -7,5 +7,5 @@ git commit -m "Сохранение_$(date +%d_%m_%Y_%H%M_S)"
 ping -c 1 8.8.8.8 > /dev/null
 if [[ $? -eq 0 ]]
 then
-    git push https://github.com/HunterXIII/Notes.git
+    git push git@github.com:HunterXIII/Notes.git
 fi
